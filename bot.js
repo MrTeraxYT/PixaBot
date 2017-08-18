c419161f7d814b58a40ecf4839ba8ec09c36a23f
heri0nd3 <blastorgeyzer93965@gmail.com>
2017-08-18 10:23:26 -0700

    About and constructing help command

--- bot.js
+++ bot.js
@@ -16,31 +16,39 @@ client.on("message", function(message) {
              const embed = new Discord.RichEmbed()
            	.setColor("#940000")
            	.setTitle(":ping_pong: Pong!", "Response time: " + client.ping + "ms")
-           	.setFooter("Requested by " + message.author.username, message.author.displayAvatarURL)
+           	.setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
            	.setTimestamp()
-		message.channel.send({embed});
-		break;
-	// about command
-	case "about":
-	     const embed = new Discord.RichEmbed()
+            message.channel.send({embed});
+            break;
+        // about command
+        case "about":
+            const embed = new Discord.RichEmbed()
 	        .setColor("#940000")
-  		.setTitle("About PixaBot", "PixaBot is a Discord bot that features games, image manipulation, moderation and music commands written in JavaScript.")
-           	.setFooter("Requested by " + message.author.username, message.author.displayAvatarURL)
+            .setTitle("About PixaBot", "PixaBot is a Discord bot that features games, image manipulation, moderation and music commands written in JavaScript.")
+           	.setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
            	.setTimestamp()
-		message.channel.send({embed});
-		break;
-	// version command
+            message.channel.send({embed});
+            break;
+        // version command
         case "version":
             message.channel.send("The current version of PixaBot is " + 0.2  + " beta 1")
             break;
+        // kick command
         case "kick":
 		    message.channel.send("Coming Soon!")
 			break;
+        // ban command
 		case "ban":
 		    message.channel.send("Coming Soon!")
 			break;
 		case "help":
-		    message.channel.send("coming soon!")
+            const embed = new Discord.RichEmbed()
+	        .setColor("#940000")
+            .setTitle("PixaBot Help Guide", "Here are my available commands. To execute one of my commands, my prefix is "px;"."")
+            .addField("General Commands", "Coming soon!")
+            .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+           	.setTimestamp()
+            message.channel.send({embed});
 			break;
 	    case "coming soon":
 		    message.channel.send("kick: px;kick ban: px;ban and help command")
