88c55d244575782777c4aeb0a6b441370b2f662e
zenrac <zenrac@outlook.fr>
2017-08-22 01:34:09 +0200

    Corrected and simplified code update

--- bot.js
+++ bot.js
@@ -65,6 +65,7 @@ client.on("message", function(message) {
     var haveMatched=true;    
  
     switch (args[0]) {
+        
         //ping command
         case "ping":
             var m = message.channel.send(":hourglass: *Testing connectivity…*").then(m => {                 
@@ -78,6 +79,7 @@ client.on("message", function(message) {
             message.channel.send({embed})
             });
             break;
+            
         // about command
         case "about":
             var embed = new Discord.RichEmbed()
@@ -89,38 +91,42 @@ client.on("message", function(message) {
            	.setTimestamp()
             message.channel.send({embed});
             break;
+            
         // version command
         case "version":
             var embed = new Discord.RichEmbed()
 	        .setColor("#940000")
-            	.setAuthor("Version", "https://cdn.discordapp.com/attachments/347288279357456387/348642213582077953/ver.png")
-		.setDescription("The current version is " + version + ".")
+            .setAuthor("Version", "https://cdn.discordapp.com/attachments/347288279357456387/348642213582077953/ver.png")
+            .setDescription("The current version is " + version + ".")
            	.setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
            	.setTimestamp()
             message.channel.send({embed});
             break;
+            
         // kick command
         case "kick":
 		    message.channel.send("Coming Soon!")
 		break;
+        
         // ban command
         case "ban":
             message.channel.send("Coming Soon!")
             break;
-	// shutdown command
+            
+        // shutdown command
         case "shutdown":
             if (message.author.id != owner_id) {
                 return;
             } else {
                 var embed = new Discord.RichEmbed()
                 .setColor("#940000")
-                    .setAuthor("Shutting down…", "https://cdn.discordapp.com/attachments/347288279357456387/348594144790183937/power.png")
-		    .setDescription("Please wait while the bot system is currently shutting down.")
-                    .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
-                    .setTimestamp()
+                .setAuthor("Shutting down…", "https://cdn.discordapp.com/attachments/347288279357456387/348594144790183937/power.png")
+                .setDescription("Please wait while the bot system is currently shutting down.")
+                .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+                .setTimestamp()
                 message.channel.send({embed});
                 client.destroy((err) => {
-                        console.log(err);
+                console.log(err);
                     });
                 console.log("The bot is now shut down.");
                 setTimeout(function(){
@@ -129,65 +135,60 @@ client.on("message", function(message) {
             }
             break;
                 //exit node.js without an error
-	// help command
+                
+        // help command
         case "help":
-                var embed = new Discord.RichEmbed()
-                .setColor("#940000")
-                .setAuthor("PixaBot Help Guide", "https://cdn.discordapp.com/attachments/347288279357456387/348643679373754369/help.png")
-				.setDescription("Here are my available commands. To execute one of my commands, my prefix is `px;`.".replace('px;', prefix))
-                .addField("General Commands", "`about` - Information about PixaBot.\n`ping` - Test a connection to PixaBot.\n`version` - Displays the PixaBot's current version.", true)
-				.addField("Fun Commands", "`8ball` - Ask a question to a bot, and what does the bot say…?\n`piko` - Shows the picture of Piko Kugihara, an anime original character serves as a mascot of PixaBot.\n`say` - Say something as a bot!")
-				.addField("Moderation Commands", "`ban` - Bans the user out of this server.\n`kick` - Kicks a member.", true)
-                .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
-                .setTimestamp()                
-                if (message.author.id == owner_id) {    
-                    embed.addField("Commands for the Bot Owner", "`shutdown` - De-initialize the bot.", true)
-                }
-                message.channel.send({embed});
+            var embed = new Discord.RichEmbed()
+            .setColor("#940000")
+            .setAuthor("PixaBot Help Guide", "https://cdn.discordapp.com/attachments/347288279357456387/348643679373754369/help.png")
+            .setDescription("Here are my available commands. To execute one of my commands, my prefix is `px;`.".replace('px;', prefix))
+            .addField("General Commands", "`about` - Information about PixaBot.\n`ping` - Test a connection to PixaBot.\n`version` - Displays the PixaBot's current version.", true)
+            .addField("Fun Commands", "`8ball` - Ask a question to a bot, and what does the bot say…?\n`piko` - Shows the picture of Piko Kugihara, an anime original character serves as a mascot of PixaBot.\n`say` - Say something as a bot!")
+            .addField("Moderation Commands", "`ban` - Bans the user out of this server.\n`kick` - Kicks a member.", true)
+            .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+            .setTimestamp()                
+            if (message.author.id == owner_id) {    
+                embed.addField("Commands for the Bot Owner", "`shutdown` - De-initialize the bot.", true)
+            }
+            message.channel.send({embed});
             break;
+            
 		// say command
 	    case "say":
 			try {
-					if(!args[0]) {
-						message.channel.send("**Error:**")
-					} else {
+                    if(message.content.length < prefix.length + 4) {
+                        message.channel.send("What i'm supposed to say ?")
+                        break
+                    } else { 
 							message.channel.send(message.content.replace(prefix + "say ", ''));
 					message.delete();
 							}
 					} catch(err) {
 						message.channel.send(error)
 					}
-			break;
-			case "8ball":
-			if(!args[0]) {
-		message.channel.send("Go ahead, ask it anything.")
-	} else {
-	switch (Math.floor(Math.random() * 100) % 7) {
-					case 0:
-					message.reply("Yes.");
-					break;
-					case 1:
-					message.reply("No.");
-					break;
-					case 2:
-					message.reply("Absolutely not.");
-					break;
-					case 3:
-					message.reply("Maybe.");
-					break;
-					case 4:
-					message.reply("It could be true.");
-					break;
-		}
-	}
-	break;
+            break;
+
+        // 8ball command    
+        case "8ball":
+			if(message.content.length < prefix.length + 6) {
+                message.channel.send("Go ahead, ask it anything.")
+                break
+            } else {        
+                var choices = ["Yes.", "No.", "Maybe.", "It could be true"]
+                var rand = choices[Math.floor(Math.random() * choices.length)];                    
+                message.channel.send(rand)  
+                break
+            }
+        
+        // piko command
 		case "piko":
 		    message.reply("Sent a DM with our mascot, Piko.");
-			message.author.send("Here's the mascot, Piko, made by Yottabyte Inside, also known as Jigs:\n https://cdn.discordapp.com/attachments/347282801021943811/348985242385907714/pixa_by_exjageroo-dbka7oa.png ")
-		break;
-	 default:
-        haveMatched = false
-	    message.channel.send(":x: *Unknown command specified. Execute `px;help` to see my list of available commands.*".replace('px;', prefix))
+            message.author.send("Here's the mascot, Piko, made by Yottabyte Inside, also known as Jigs.", { files: [ 'https://cdn.discordapp.com/attachments/347282801021943811/348985242385907714/pixa_by_exjageroo-dbka7oa.png' ] });
+            break;
+        
+        default:
+            haveMatched = false
+            message.channel.send(":x: *Unknown command specified. Execute `px;help` to see my list of available commands.*".replace('px;', prefix))
 }
 if (haveMatched){
     console.log(`[Command] ${message.author.id}/${message.author.username} (${message.content})`)
