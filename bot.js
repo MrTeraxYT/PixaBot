e8d515cbdfe6bd93a2c8ae0a27c98002f721965e
heri0nd3 <blastorgeyzer93965@gmail.com>
2017-08-22 09:28:53 -0700

    revert more

--- bot.js
+++ bot.js
@@ -234,7 +234,6 @@ var guild = msg.guild
 	  .addField("📥 Input", args.join(' '))
 	  .addField("📤 Output", "```x1\n" + clean(evaled) + "```")
       message.channel.send({embed});
-	  break
 	  console.log("========================================================================================\nEVAL RESULTS\nINPUT:\n" + args.join(' ') + "\nOUTPUT:\n" + clean(evaled) + "\n========================================================================================\n");
     } catch (err) {
 	message.delete()
